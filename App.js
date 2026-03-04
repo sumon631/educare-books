fc6cf367bbe9c5607ed3ad460472c6890f676db5
sumon631 <138502329+sumon631@users.noreply.github.com>
2026-03-05 05:21:23 +0600

    Initial commit of App.js

--- App.js
+++ App.js
@@ -1,1 +1,13 @@
-// This is your App.js file content.
+// App.js
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the EduCare Books Application</h1>
+    </div>
+  );
+}
+
+export default App;
